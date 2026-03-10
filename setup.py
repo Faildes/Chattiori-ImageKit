3533ac43a0b41d64f194190d7b39f810302b90cd
Chattiori / Team-C <127287320+Faildes@users.noreply.github.com>
2026-03-10 12:27:04 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+setup(
+    name               = 'chattiori_imagekit'
+    , version          = '1.0'
+    , license          = 'Apache License'
+    , author           = "Chattiori"
+    , packages         = find_packages('src')
+    , package_dir      = {'': 'src'}
+    , url              = 'https://github.com/Faildes/Chattiori_ImageKit'
+    , keywords         = 'diffusers stable-diffusion pil image'
+    , install_requires = [
+        'pillow'
+    ]
+    , include_package_data=True
+)
